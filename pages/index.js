3a4d18b574b68541820c0f6f242afc5c150fb5d7
ITyugaev <igor.tyugaev@gmail.com>
2022-07-31 17:19:27 +0500

    Implemend DropDown component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import "styles/global.style";
-import {TextArea} from "../components/ui/textArea";
+import {DropDown} from "../components/ui/dropDown";
+import {useState} from "react";
+
+const options = [
+    {
+        label: 'color grading',
+        value: 1
+    },
+    {
+        label: 'VFX-design',
+        value: 2
+    },
+    {
+        label: 'Dailies',
+        value: 3
+    }
+]
 
 const Home = () => {
+    const [value, setValue] = useState(null);
     return (
         <>
             <main style={{height: '100vh', overflow: "visible"}}>
-                <TextArea/>
+                <DropDown title="Service" onChange={(option) => setValue(option)} options={options} value={value}/>
             </main>
         </>
     );
