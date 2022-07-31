cdfa4931edadbeead366696f4557d6e5dff49202
ITyugaev <igor.tyugaev@gmail.com>
2022-07-31 18:05:06 +0500

    Add title for textArea

--- components/ui/textArea/textArea.style.js
+++ components/ui/textArea/textArea.style.js
@@ -14,22 +14,21 @@ export const InputWrapper = styled.div`
 const lineHeight = 90
 export const TextAreaWrapper = styled('textarea')(({isError}) => ({
 
-        width: '100%',
-        minHeight: lineHeight * 4 + 'px',
-        overflow: 'hidden',
-        textTransform: 'uppercase',
+    width: '100%',
+    minHeight: lineHeight * 4 + 'px',
+    overflow: 'hidden',
+    textTransform: 'uppercase',
 
-        resize: 'none',
-        fontSize: fontSizes.desktopLG.h3,
-        color: isError ? colors.red : colors.yellow,
-        fontFamily: fontFamilies.mainFont,
+    resize: 'none',
+    fontSize: fontSizes.desktopLG.h3,
+    color: isError ? colors.red : colors.yellow,
+    fontFamily: fontFamilies.mainFont,
 
-        lineHeight: lineHeight + 'px',
-        backgroundImage: `linear-gradient(transparent, transparent calc(${lineHeight}px - 2px), ${isError ? colors.red : colors.white} 0px)`,
-        backgroundSize: `100% ${lineHeight}px`,
-        backgroundPositionY: `${-0.2 * lineHeight}px`,
-    }))
-;
+    lineHeight: lineHeight + 'px',
+    backgroundImage: `linear-gradient(transparent, transparent calc(${lineHeight}px - 2px), ${isError ? colors.red : colors.white} 0px)`,
+    backgroundSize: `100% ${lineHeight}px`,
+    backgroundPositionY: `${-0.2 * lineHeight}px`,
+}));
 
 export const Error = styled.div`
   width: 100%;
@@ -40,5 +39,13 @@ export const Error = styled.div`
   font-family: ${fontFamilies.subFont};
 
   ${getCurrentFontSizeStyle("caption")};
+`;
+
+export const Title = styled.h3`
+  text-transform: uppercase;
+  font-size: ${fontSizes.desktopLG.h3};
+  font-family: ${fontFamilies.mainFont};
+  color: ${({isError}) => isError ? colors.red : colors.white};
+  margin-bottom: 8px;
 `;
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import "styles/global.style";
-import {DropDown} from "../components/ui/dropDown";
 import {useState} from "react";
+import {DropDown} from "../components/ui/dropDown";
+import {TextArea} from "../components/ui/textArea";
+import "styles/global.style";
 
 const options = [
     {
@@ -23,6 +24,10 @@ const Home = () => {
         <>
             <main style={{height: '100vh', overflow: "visible"}}>
                 <DropDown title="Service" onChange={(option) => setValue(option)} options={options} value={value}/>
+
+                <TextArea title="About your work" onChange={(val) => {
+                    console.log(val)
+                }}/>
             </main>
         </>
     );
