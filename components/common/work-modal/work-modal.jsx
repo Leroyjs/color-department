216306311efb8379c0b977a9490fdbec1142910d
SergeiKolesnikovAdd <entaroartansis@gmail.com>
2022-08-07 21:24:36 +0500

    added button link

--- components/common/work-modal/work-modal.jsx
+++ components/common/work-modal/work-modal.jsx
@@ -2,6 +2,7 @@ import Modal from "../modal";
 import { ButtonEllipse } from "components";
 import { H2 } from "components";
 import { GratitudeWrapper, StyledH2 } from "./work-modal.style";
+import Link from "next/link";
 
 export const WorkModal = () => {
   return (
@@ -10,8 +11,12 @@ export const WorkModal = () => {
         <StyledH2 color="white">Thank you for your application,</StyledH2>
         <StyledH2 color="primary" mb="md">
           we will contact you shortly
-        </StyledH2>
-        <ButtonEllipse>OK</ButtonEllipse>
+        </StyledH2><Link href="/">
+        <ButtonEllipse>
+          
+            <a>OK</a>
+          
+        </ButtonEllipse></Link>
       </GratitudeWrapper>
     </Modal>
   );
