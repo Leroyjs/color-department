b73c66e5090f8955daba7d2a6808fb125dfb906b
ITyugaev <igor.tyugaev@gmail.com>
2022-08-07 15:35:00 +0500

    Add example with playing controlled by hover event

--- components/common/videoBackground/index.js
+++ components/common/videoBackground/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {VideoContainer} from "./video.style";
 import {PropTypes} from "prop-types";
 
-const VideoBackground = ({poster, video, ...props}) => {
+// eslint-disable-next-line react/display-name
+const VideoBackground = forwardRef(({poster, video, autoPlay = true, ...props}, ref) => {
     return (
-        <VideoContainer playsInline autoPlay muted loop poster={poster} {...props}>
+        <VideoContainer ref={ref} playsInline autoPlay={autoPlay} muted loop controls={false}
+                        poster={poster} {...props}>
             {video?.webm && <source src={video.webm} type="video/webm"/>}
             <source src={video.mp4} type="video/mp4"/>
         </VideoContainer>
     );
-};
+});
 
 export default VideoBackground;
 
@@ -17,6 +19,7 @@ const SourceType = PropTypes.oneOfType([PropTypes.string, PropTypes.object]);
 
 VideoBackground.propTypes = {
     poster: SourceType.isRequired,
+    autoPlay: PropTypes.bool,
     video: PropTypes.shape({
         webm: SourceType,
         mp4: SourceType.isRequired,
