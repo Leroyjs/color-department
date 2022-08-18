1472154fc5a160740f43a6eca94ee704f1fa152d
Bysooft <gruzdov.code@gmail.com>
2022-08-18 16:44:08 +0500

    added dropdowns at video-grid

---
+++ components/sections/video-grid/constant.js
@@ -0,0 +1,81 @@
+export const gridItems = [
+    {
+        video:
+            "https://user-images.githubusercontent.com/70715305/174163866-72e76540-1982-4e8a-bb40-383cf3ca2491.mp4",
+        poster:
+            "https://phonoteka.org/uploads/posts/2021-05/1622021938_9-phonoteka_org-p-pop-arti-krasivo-11.jpg",
+        id: 1,
+        genre: "Comedy",
+        category: "Movie",
+        colourist: "SEGA MEGA DRIVE",
+    },
+    {
+        video:
+            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4	",
+        poster:
+            "https://static.tildacdn.com/tild6363-6630-4436-a132-633033393666/Pop-Art-sochny-j.jpg",
+        id: 9,
+        genre: "Drama",
+        category: "Movie",
+        colourist: "SEGA MEGA DRIVE",
+    },
+    {
+        video:
+            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
+        poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
+        id: 2,
+        genre: "Comedy",
+        category: "Clip",
+        colourist: "SEGA MEGA DRIVE",
+    },
+    {
+        video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+        poster:
+            "https://proprikol.ru/wp-content/uploads/2021/02/sychi-krasivye-kartinki-5.jpg",
+        id: 3,
+        genre: "Drama",
+        category: "Clip",
+        colourist: "SEGA MEGA DRIVE",
+    },
+    {
+        video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        poster:
+            "https://phonoteka.org/uploads/posts/2021-05/1622021938_9-phonoteka_org-p-pop-arti-krasivo-11.jpg",
+        id: 4,
+        genre: "Comedy",
+        category: "Movie",
+        colourist: "SEGA MEGA DRIVE",
+    },
+    {
+        video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
+        id: 5,
+        genre: "Fantastic",
+        category: "Movie",
+        colourist: "SEGA MEGA DRIVE",
+    },
+    {
+        video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+        poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
+        id: 6,
+        genre: "Comedy",
+        category: "Movie",
+        colourist: "Dmitrix",
+    },
+    {
+        video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+        poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
+        id: 7,
+        genre: "Comedy",
+        category: "Film",
+        colourist: "Dmitrix",
+    },
+    {
+        video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+        poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
+        id: 8,
+        genre: "Horror",
+        category: "Film",
+        colourist: "Dmitrix",
+    },
+];
--- components/sections/video-grid/video-grid.jsx
+++ components/sections/video-grid/video-grid.jsx
@@ -1,109 +1,77 @@
 import { VideoGridWrapper } from "./video-grid.style";
 import { VideoBackground } from "components";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { VideoGridItem } from "components";
-
-
-
+import { gridItems } from "./constant";
+import { DropDown } from "components";
 
 export const VideoGrid = () => {
+  const videoRef = useRef();
+  const [genre, setGenre] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [colourist, setColourist] = useState(null);
 
- const gridItems = [
-   {
-     video:
-       "https://user-images.githubusercontent.com/70715305/174163866-72e76540-1982-4e8a-bb40-383cf3ca2491.mp4",
-     poster:
-       "https://phonoteka.org/uploads/posts/2021-05/1622021938_9-phonoteka_org-p-pop-arti-krasivo-11.jpg",
-     id: 1,
-   },
-   {
-     video:
-       "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4	",
-     poster:
-       "https://static.tildacdn.com/tild6363-6630-4436-a132-633033393666/Pop-Art-sochny-j.jpg",
-     id: 9,
-   },
-   {
-     video:
-       "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
-     poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
-     id: 2,
-   },
-   {
-     video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
-     poster:
-       "https://proprikol.ru/wp-content/uploads/2021/02/sychi-krasivye-kartinki-5.jpg",
-     id: 3,
-   },
-   {
-     video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-     poster:
-       "https://phonoteka.org/uploads/posts/2021-05/1622021938_9-phonoteka_org-p-pop-arti-krasivo-11.jpg",
-     id: 4,
-   },
-   {
-     video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-     poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
-     id: 5,
-   },
-   {
-     video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
-     poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
-     id: 6,
-   },
-   {
-     video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
-     poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
-     id: 7,
-   },
-   {
-     video: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
-     poster: "https://i.ytimg.com/vi/kYRUg7Vfits/maxresdefault.jpg",
-     id: 8,
-   },
- ];
-
-
- const videoRef = useRef();
-
- function handleEnter(event) {
-   const { target } = event;
-   if (target.currentTime > 0.001) target.play();
- }
-
- function handleLeave(event) {
-   const { target } = event;
-   target.pause();
- }
-
- function handlePlay() {
-   const videoEl = videoRef.current;
-   if (!videoEl) return;
-   videoEl.play();
+  function handleEnter(event) {
+    const { target } = event;
+    if (target.currentTime > 0.001) target.play();
   }
 
-const VideoList =	gridItems.map((item) => (
+  function handleLeave(event) {
+    const { target } = event;
+    target.pause();
+  }
 
-  <VideoGridItem
-    poster={item.poster}
-            video={{mp4: item.video}}
-            key={item.id}
-            autoPlay={false}
-            ref={videoRef}
-            onClick={handlePlay}
-            onMouseEnter={handleEnter}
-            onMouseLeave={handleLeave}/>
+  function handlePlay() {
+    const videoEl = videoRef.current;
+    if (!videoEl) return;
+    videoEl.play();
+  }
 
-	))
-
-
-
-
-
-	return (
-
-		<VideoGridWrapper>
-			{VideoList}
-		</VideoGridWrapper>
-	);
+  const genreFilter = [];
+  const categoryFilter = [];
+  const colouristFilter = [];
+  const VideoList = gridItems.map(
+    ({ poster, video, id, genre, category, colourist }) => (
+      genreFilter.push(genre),
+      categoryFilter.push(category),
+      colouristFilter.push(colourist),
+      (
+        <VideoGridItem
+          poster={poster}
+          video={{ mp4: video }}
+          key={id}
+          autoPlay={false}
+          ref={videoRef}
+          onClick={handlePlay}
+          onMouseEnter={handleEnter}
+          onMouseLeave={handleLeave}
+        />
+      )
+    )
+  );
+  new Set(genreFilter);
+  return (
+    <>
+      {" "}
+      <DropDown
+        title="All categories"
+        onChange={(option) => setCategory(option)}
+        options={new Set(categoryFilter)}
+        value={category}
+      />
+      <DropDown
+        title="All genres"
+        onChange={(option) => setGenre(option)}
+        options={new Set(genreFilter)}
+        value={genre}
+      />
+      <DropDown
+        title="Select corourist"
+        onChange={(option) => setColourist(option)}
+        options={new Set(colouristFilter)}
+        value={colourist}
+      />
+      <VideoGridWrapper>{VideoList}</VideoGridWrapper>
+    </>
+  );
 };
