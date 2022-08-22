239402fd0b378aa7d28950b513dc58e74cb15843
Bysooft <gruzdov.code@gmail.com>
2022-08-22 16:38:42 +0500

    partial adaptive modal-shop

--- components/common/shop-modal/shop-modal.style.js
+++ components/common/shop-modal/shop-modal.style.js
@@ -4,18 +4,35 @@ import {
   getCurrentPaddingStyle,
   getCurrentBackgroundColorStyles,
 } from "styles";
-import {H3} from "../../ui/h3";
-import {Text} from "../../ui/text";
+import { H3 } from "../../ui/h3";
+import { Text } from "../../ui/text";
 
 export const ShopModalInner = styled.div`
   width: 100%;
   display: flex;
   height: 100vh;
+
+  @media (max-width: 768px) {
+    flex-direction:column;
+  }
 `;
 
 export const ProductImage = styled.div`
   width: 50%;
   ${({ theme }) => getCurrentBackgroundColorStyles("primary", theme)};
+
+  @media (max-width: 768px) {
+    width:100%;
+    height:100%;
+  }
+`;
+export const InformationItem = styled.div`
+
+ @media (max-width: 768px) {
+  width: 100%;
+    flex-direction: row;
+    display: flex;
+ }
 `;
 
 export const InformationColumn = styled.div`
@@ -24,6 +41,13 @@ export const InformationColumn = styled.div`
   ${getCurrentPaddingStyle("bottom", "md")}
   ${getCurrentPaddingStyle("top", "lg")}
   ${({ theme }) => getCurrentBackgroundColorStyles("black", theme)};
+
+  @media (max-width: 768px) {
+    display: flex;
+    width:100%;
+    flex-direction: column;
+    height: 100%;
+  }
 `;
 
 export const TextContainer = styled.div`
@@ -40,8 +64,16 @@ export const ProductDiscription = styled(Text)`
   ${({ theme }) => getCurrentColorStyles("white", theme)};
   ${getCurrentPaddingStyle("bottom", "lg")};
   ${getCurrentPaddingStyle("top", "md")};
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const FormWrapper = styled.form`
   ${({ theme }) => getCurrentBackgroundColorStyles("black", theme)}
+
+  @media (max-width: 768px) {
+  width: 100%;
+  }
 `;
