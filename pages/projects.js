b1a26449eaa8ddbfed72220b7a08094bbf63b23f
ITyugaev <igor.tyugaev@gmail.com>
2022-08-25 23:46:18 +0500

    Кусок говна

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,8 @@
-import "styles/global.style";
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import {ProjectCards, RunningLine} from "components";
-
-const Projects = () => {
+import "styles/global.style";
+const Projects = ({data}) => {
+    console.log(data)
 
     return (
         <>
@@ -12,3 +13,30 @@ const Projects = () => {
 };
 
 export default Projects;
+
+export async function getStaticProps() {
+    const client = new ApolloClient({
+        uri: 'https://api.cd.rrn.su/',
+        cache: new InMemoryCache()
+    });
+
+    const { data } = await client.query({
+        query: gql`query {
+              projects {
+                id
+                title
+                description
+                year
+                photoUrl
+                videoUrl
+                previewVideoUrl
+              }
+        }`
+    });
+
+    return {
+        props: {
+            data
+        }
+    }
+}
