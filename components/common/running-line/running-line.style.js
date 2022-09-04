04d94f745695e37f760fd99a10f5a5b769da67df
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-09-04 13:54:21 +0500

    fix govna

--- components/common/running-line/running-line.style.js
+++ components/common/running-line/running-line.style.js
@@ -1,17 +1,25 @@
-import { keyframes } from "@emotion/css"
-import styled from "@emotion/styled"
-import { getVW } from "styles"
-import { breakpointsWidth } from "styles"
-import { applyMargins, applyPaddings, colors, getCurrentColor, getCurrentMarginStyle, sizes } from "styles"
+import {
+  applyMargins,
+  applyPaddings,
+  colors,
+  getCurrentColor,
+  getCurrentMarginStyle,
+  sizes,
+} from "styles";
+
+import { breakpointsWidth } from "styles";
+import { getVW } from "styles";
+import { keyframes } from "@emotion/css";
+import styled from "@emotion/styled";
 
 const scrollAnimation = keyframes`
     from { transform: translateX(0);}
     to { transform: translateX(-50%); }
-`
+`;
 
 const ScrollContentDinamicStyle = ({ outline }) => {
-    if (outline) {
-        return `
+  if (outline) {
+    return `
             & h1 span {
                 color: ${colors.black};
                 text-shadow: 
@@ -21,87 +29,90 @@ const ScrollContentDinamicStyle = ({ outline }) => {
                     1.5px 1.5px 0 ${colors.white};
                 transition: color 0.3s, text-shadow 0.3s;
             }
-        `
-    }
-}
+        `;
+  }
+};
 
 export const RunningLineScrollContent = styled.div`
-    display: flex;
-    align-items: center;
-    white-space: nowrap;
-    ${ScrollContentDinamicStyle};
-`
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  ${ScrollContentDinamicStyle};
+`;
 
 const SeparatorDinamicStyle = ({ outline }) => {
-    if (outline) {
-        return `
+  if (outline) {
+    return `
             & span { 
                 color: ${colors.white};
                 transition: color 0.3s;
             }
             border-color: ${colors.white};
             transition: border-color 0.3s;
-        `
-    }
-}
+        `;
+  }
+};
 
 export const RunningLineSeparator = styled.div`
-    border: 1px solid;
-    border-radius: 50%;
-    border-color: ${({ theme }) => getCurrentColor("primary", theme)};
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    @media screen and (min-width: ${breakpointsWidth.desktopLG}) {
-        height: ${getVW(100)};
-        min-width: ${getVW(240)};
-    }
-    @media screen and (max-width: ${breakpointsWidth.desktopLG}) {
-        height: ${sizes['desktopLG'].half};
-        min-height: 100px;
-        min-width: 240px;
-    }
-    @media screen and (max-width: ${breakpointsWidth.tabletLG}) {
-        height: ${sizes['tabletLG'].half};
-        min-width: 200px;
-    }
-    @media screen and (max-width: ${breakpointsWidth.phone}) {
-        min-height: 65px;
-    }
-    ${getCurrentMarginStyle('horizontal', 'sm')}
-    ${SeparatorDinamicStyle};
-`
+  border: 1px solid;
+  border-radius: 50%;
+  border-color: ${({ theme }) => getCurrentColor("primary", theme)};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  @media screen and (min-width: ${breakpointsWidth.desktopLG}) {
+    height: ${getVW(100)};
+    min-width: ${getVW(240)};
+  }
+  @media screen and (max-width: ${breakpointsWidth.desktopLG}) {
+    height: ${sizes["desktopLG"].half};
+    min-height: 100px;
+    min-width: 240px;
+  }
+  @media screen and (max-width: ${breakpointsWidth.tabletLG}) {
+    height: ${sizes["tabletLG"].half};
+    min-width: 200px;
+  }
+  @media screen and (max-width: ${breakpointsWidth.phone}) {
+    min-height: 65px;
+  }
+  ${getCurrentMarginStyle("horizontal", "sm")}
+  ${SeparatorDinamicStyle};
+`;
 export const RunningLineWrapper = styled.div`
-    overflow: hidden;
-    display: flex;
-    width: 100%;
-    margin-left: -50%;
-    translateZ(0);
-    transform: translateX(50%);
-  
-    ${({ link }) => link ? "cursor: pointer" : ""};
-    &>*:nth-of-type(1n){
-        animation: ${scrollAnimation} 15s linear infinite;
-    }
-    :hover {
-        h1 span {
-            color: ${({ theme }) => getCurrentColor("primary", theme)};
-            text-shadow: 
-                    -1.5px -1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)}, 
-                    1.5px -1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)}, 
-                    -1.5px 1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)}, 
-                    1.5px 1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)};
-        }
+  overflow: hidden;
+  display: flex;
+  width: 100%;
+  margin-left: -50%;
+  align-items: center;
+  height: 13vw;
+  transform: translateX(50%) translateZ(0);
 
-        ${RunningLineSeparator}{
-            border-color: ${({ theme }) => getCurrentColor("primary", theme)};
-            & span {
-                color: ${({ theme }) => getCurrentColor("primary", theme)};
-            }
-        }
-    
+  @media screen and (max-width: ${breakpointsWidth.tabletLG}) {
+    height: 24.5vw;
+  }
+
+  ${({ link }) => (link ? "cursor: pointer" : "")};
+  & > *:nth-of-type(1n) {
+    animation: ${scrollAnimation} 15s linear infinite;
+  }
+  :hover {
+    h1 span {
+      color: ${({ theme }) => getCurrentColor("primary", theme)};
+      text-shadow: -1.5px -1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)},
+        1.5px -1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)},
+        -1.5px 1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)},
+        1.5px 1.5px 0 ${({ theme }) => getCurrentColor("primary", theme)};
     }
-    ${applyMargins};
-    ${applyPaddings}
-`
+
+    ${RunningLineSeparator} {
+      border-color: ${({ theme }) => getCurrentColor("primary", theme)};
+      & span {
+        color: ${({ theme }) => getCurrentColor("primary", theme)};
+      }
+    }
+  }
+  ${applyMargins};
+  ${applyPaddings}
+`;
