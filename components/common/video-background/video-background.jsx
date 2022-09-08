af0987f1083b474576c717600b2cac173382a355
ITyugaev <igor.tyugaev@gmail.com>
2022-09-08 22:51:07 +0500

    fix imgs

--- components/common/video-background/video-background.jsx
+++ components/common/video-background/video-background.jsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react'
 import { propTypesVideoBg } from '../../types'
-import { VideoContainer } from './video.style'
+import { VideoContainer, VideoStyle } from './video.style'
 
 // eslint-disable-next-line react/display-name
 export const VideoBackground = forwardRef(
@@ -30,12 +30,11 @@ VideoBackground.propTypes = {
   ...propTypesVideoBg,
 }
 
-
 export function EmbedVideo(props) {
-    return (
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
         <video
           loop
           muted
@@ -46,7 +45,7 @@ export function EmbedVideo(props) {
          <source src="https://drive.google.com/uc?id=1bUpQserRqEPb72Yqt-jawGHj1-LB4amq" type="video/mp4"/>
 </video>
       `,
-        }}
-      ></div>
-    );
+      }}
+    ></div>
+  )
 }
